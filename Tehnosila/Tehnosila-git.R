77a6c60353aade149748d7f6f056666fea87d1da
Nikolay Aleksandrovich <glushkov@realweb.ru>
2015-06-05 14:12:16 +0300

    edit TS

--- Tehnosila/Tehnosila-git.R
+++ Tehnosila/Tehnosila-git.R
@@ -5,9 +5,19 @@ library("sqldf")
 
 ga_token <- authorize(client.id = "44510395713-cngu0lhjk5rud2o8s3noj6qj17f7obng.apps.googleusercontent.com", client.secret = "FHsO_3tCeb4FgFXjw-GPbCTW")
 
-TS <- get_ga(profile.id = "44835839",
-                    start.date = "2015-04-20", 
-                    end.date = "2015-04-20", 
-                    metrics = "ga:hits,ga:sessions,ga:goal6Completions", 
-                    dimensions = "ga:dimension11,ga:dimension12,ga:dimension13,ga:sourceMedium,ga:city,ga:landingPagePath",
+
+dateStart <-"2015-04-23"
+dateEnd <-"2015-04-26"
+dates <- seq(as.Date(dateStart), as.Date(dateEnd), by = "days")
+
+TSrawData <- do.call(
+  rbind, lapply(dates, function(d) {
+    tehnosila <- get_ga(profile.id = "100821824",
+                        start.date = d, 
+                        end.date = d, 
+                        metrics = "ga:hits,ga:transactions,ga:productAddsToCart", 
+                        dimensions = "ga:dimension1,ga:dimension2,ga:dimension3,ga:dimension4,ga:sourceMedium,ga:city,ga:pagePath"
+                        
+    )
+  })
 )
